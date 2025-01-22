883503e0404e9c10e479e7a4791a04ab64244ecc
Alex van Steenhoven <alex@modus-digital.com>
2025-01-22 14:41:47 +0100

    feature(service provider) Added a install command and added asset serving

--- src/ModusUIServiceProvider.php
+++ src/ModusUIServiceProvider.php
@@ -4,6 +4,8 @@ namespace ModusDigital\ModusUI;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use ModusDigital\ModusUI\Commands\InstallCommand;
+use Composer\InstalledVersions;
 
 final class ModusUIServiceProvider extends ServiceProvider
 {
@@ -12,6 +14,14 @@ final class ModusUIServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'modus-ui');
         $this->registerComponents();
         $this->registerComponentPublishers();
+
+        $this->handleAssetServing();
+        
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallCommand::class,
+            ]);
+        }
     }
 
     public function register()
@@ -22,6 +32,8 @@ final class ModusUIServiceProvider extends ServiceProvider
     private function registerComponents(): self
     {
         Blade::componentNamespace('ModusDigital\\ModusUI\\View\\Components\\Form', config('modus-ui.prefix'));
+
+        Blade::component('modus-ui::toasts', config('modus-ui.prefix').'::toasts');
 
         return $this;
     }
@@ -36,11 +48,45 @@ final class ModusUIServiceProvider extends ServiceProvider
 
         // Views
         $this->publishes(
-            paths: [ __DIR__.'/../resources/views' => resource_path('views/vendor/modus/modus-ui') ], 
+            paths: [ __DIR__.'/../resources/views' => resource_path('views/components') ], 
             groups: 'modus-ui-views'
+        );
+
+        // Js Assets
+        $this->publishes(
+            paths: [ __DIR__.'/../resources/js' => public_path('vendor/modus/modus-ui') ], 
+            groups: 'modus-ui-assets'
         );
 
         return $this;
     }
-}
 
+    private function handleAssetServing()
+    {
+        Blade::directive('modusUiScripts', function () {
+            $version = InstalledVersions::getPrettyVersion('modus-digital/modus-ui');
+            $scripts = [];
+            
+            if (is_file(__DIR__ . '/../resources/hot')) {
+                $url = rtrim(file_get_contents(__DIR__ . '/../resources/hot'));
+
+                foreach (glob(__DIR__ . '/../resources/ts/*.ts') as $file) {
+                    $relativePath = str_replace(__DIR__ . '/../resources/ts/', '', $file);
+                    $scripts[] = sprintf('<script type="module" src="%s"></script>', "{$url}/resources/ts/{$relativePath}");
+                }
+
+                $scripts[] = sprintf('<script type="module" src="%s" defer></script>', "{$url}/@vite/client");
+            } else {
+                foreach (glob(__DIR__ . '/../resources/dist/*.js') as $file) {
+                    $relativePath = str_replace(__DIR__ . '/../resources/dist/', '', $file);
+                    $scripts[] = sprintf('<script type="module" src="%s"></script>', asset('vendor/modus/modus-ui/'.$relativePath.'?v='.$version));
+                }
+            }
+
+            // Config
+            $scripts[] = sprintf('<script>window.ModusUIConfig = %s</script>', json_encode(config('modus-ui')));
+
+            return implode("\n", $scripts);
+        });
+    }
+}
