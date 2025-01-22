325bf16a5a7d99ff1dae522955e8a2b499e6417d
Alex van Steenhoven <alex@modus-digital.com>
2025-01-22 14:51:37 +0100

    feature(components > input) Added a textarea component

---
+++ src/View/Components/Form/Textarea.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ModusDigital\ModusUI\View\Components\Form;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class Textarea extends Component
+{
+    public function __construct(
+        public string $id = '',
+        public string $name = '',
+        public string $label = '',
+        public string $value = '',
+        public string $placeholder = '',
+        public bool $disabled = false,
+        public bool $required = false,
+        public int $rows = 2
+    ) {}
+
+    public function render(): View|Closure|string
+    {
+        return view('modus-ui::form.textarea', [
+            'color' => config('modus-ui.primary_color'),
+            'id' => $this->id,
+            'name' => $this->name,
+            'label' => $this->label,
+            'placeholder' => $this->placeholder,
+            'value' => $this->value,
+            'disabled' => $this->disabled,
+            'required' => $this->required,
+            'rows' => $this->rows,
+        ]);
+    }
+}
