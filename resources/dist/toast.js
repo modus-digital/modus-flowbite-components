f6144f47d7f7fc419fa1dc37c271c31ad88b77a3
Alex van Steenhoven <alex@modus-digital.com>
2025-02-24 13:05:00 +0100

    chore(update > packages) Updated packages and ran build for frontend

--- resources/dist/toast.js
+++ resources/dist/toast.js
@@ -1,1 +1,1 @@
-window.toast=function(e,t){window.dispatchEvent(new CustomEvent("toast",{detail:{message:e,title:t.title||null,level:t.level||"info"}}))};
+window.toast=function(e,t){window.dispatchEvent(new CustomEvent("toast",{detail:{message:e,title:t.title||null,type:t.type||"info"}}))};
